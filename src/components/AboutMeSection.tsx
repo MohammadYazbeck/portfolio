8c674cbd28802315ef777672cb27e81f0ebd5cc8
Mohammad Yazbeck <mohammadyazbeckk@gmail.com>
2024-12-28 20:21:45 +0200

    update image shape

--- src/components/AboutMeSection.tsx
+++ src/components/AboutMeSection.tsx
@@ -4,12 +4,15 @@ export default function AboutMeSection() {
       id="about-me"
       className="flex h-full w-full flex-col items-center justify-start pt-[20%] sm:pt-[15%] lg:h-full lg:flex-row lg:justify-center lg:space-x-16 lg:pb-[25%]"
     >
-      <div className="mt-4 h-[15rem] w-[15rem] sm:h-[17rem] sm:w-[17rem] lg:mt-0 lg:h-[25rem] lg:w-[25rem]">
-        <img
-          src="/portfolio/images/pic.jpg"
-          className="animate-lrSmall rounded-full border border-blue-gray-900 shadow-2xl shadow-gray-300 sm:animate-lr dark:shadow-white/10"
-        />
+      <div className="mt-4 flex h-[15rem] w-[15rem] items-center justify-center sm:h-[17rem] sm:w-[17rem] lg:mt-0 lg:h-[25rem] lg:w-[25rem]">
+        <div className="flex h-full w-full transform items-center justify-center overflow-hidden rounded-[65%/70%_39%_80%_50%] border border-blue-gray-900 shadow-2xl shadow-gray-300 drop-shadow-[0px_15px_3px_rgba(0,0,0,0.25)] sm:animate-lr dark:shadow-gray-800/80">
+          <img
+            src="/portfolio/images/pic.jpg"
+            className="h-full w-full object-cover"
+          />
+        </div>
       </div>
+
       <div className="animate-rlSmall px-8 sm:mt-8 sm:animate-rl lg:h-[27rem] lg:w-[35rem] xl:w-[40rem]">
         <p className="ml-2 mt-12 text-2xl font-semibold italic tracking-tight sm:ml-5 md:ml-16 lg:ml-0 lg:mt-40 lg:text-4xl dark:text-white">
           {" "}
